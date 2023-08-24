988baa6b20dd974336c7e6c72eb07fd54511e54d
Ying Lu <yinglu5374@gmail.com>
2023-08-24 19:34:26 -0400

    add new todo

--- src/components/AddToDo.tsx
+++ src/components/AddToDo.tsx
@@ -1,15 +1,36 @@
-import { FunctionComponent } from "react";
-import "./AddToDo.css";
-const AddToDo: FunctionComponent = () => {
+import { FunctionComponent, useState } from 'react';
+import './AddToDo.css';
+
+type Props = {
+  addTodo: (name: string) => void;
+};
+
+const AddToDo: FunctionComponent<Props> = (props) => {
+  const [todo, setTodo] = useState('');
+
+  const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTodo(event.target.value);
+  };
+
+  const handleKeyUp = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter' && todo.trim().length > 0) {
+      props.addTodo(todo);
+      setTodo('');
+    }
+  };
+
   return (
     <input
-      className="add-to-do"
-      type="text"
-      placeholder="Add to do..."
+      className='add-to-do'
+      type='text'
+      placeholder='Add to do...'
       maxLength={50}
       minLength={2}
       required
       autoFocus
+      value={todo}
+      onChange={handleOnChange}
+      onKeyUp={handleKeyUp}
     />
   );
 };
--- src/components/ToDoListDesign.tsx
+++ src/components/ToDoListDesign.tsx
@@ -1,19 +1,38 @@
-import { FunctionComponent } from "react";
-import Header from "./Header";
-import ToDoComplete from "./ToDoComplete";
-import ToDoIncomplete from "./ToDoIncomplete";
-import AddToDo from "./AddToDo";
-import "./ToDoListDesign.css";
+import { FunctionComponent, useState } from 'react';
+import Header from './Header';
+import ToDoComplete from './ToDoComplete';
+import ToDoIncomplete from './ToDoIncomplete';
+import AddToDo from './AddToDo';
+import './ToDoListDesign.css';
+
+type Todo = {
+  name: string;
+  completed: boolean;
+};
 const ToDoListDesign: FunctionComponent = () => {
+  const [todoList, setTodoList] = useState<Todo[]>([] as Todo[]);
+
+  const addNewTodo = (name: string) => {
+    const newTodo = {
+      name,
+      completed: false,
+    };
+
+    const updatedTodoList = [...todoList, newTodo];
+    console.log(updatedTodoList);
+
+    setTodoList(updatedTodoList);
+  };
+
   return (
-    <div className="to-do-list-design">
-      <div className="todolistcontainer">
+    <div className='to-do-list-design'>
+      <div className='todolistcontainer'>
         <Header />
-        <div className="divider" />
+        <div className='divider' />
         <ToDoComplete />
         <ToDoIncomplete />
-        <div className="divider" />
-        <AddToDo />
+        <div className='divider' />
+        <AddToDo addTodo={addNewTodo} />
       </div>
     </div>
   );
